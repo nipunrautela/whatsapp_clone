3b2bfe33e12fc99218882eaa6213928eae03e710
Nipun Rautela <nipunrautela261@gmail.com>
2021-11-19 11:06:28 +0530

    Fixes to chat screen pixel overflows

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -15,52 +15,44 @@ class _ChatScreenState extends State<ChatScreen> {
     Container chat = Container(
       width: MediaQuery.of(context).size.width,
       padding: const EdgeInsets.all(1.0),
-      child: Row(
-        children: [
-          const Divider(
-            indent: 4.0,
+      child: ListTile(
+        leading: const CircleAvatar(
+          backgroundColor: Colors.white,
+          backgroundImage: NetworkImage(
+            'https://cdn.discordapp.com/attachments/775387982554333204/911101173082247168/download.png',
+            scale: 1.0,
           ),
-          const CircleAvatar(
-            backgroundColor: Colors.white,
-            backgroundImage: NetworkImage(
-              'https://cdn.discordapp.com/attachments/775387982554333204/911101173082247168/download.png',
-              scale: 1.0,
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              "Somebody",
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          const Divider(
-            indent: 10.0,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Divider(
-                height: 15.0,
+            Text(
+              "9:10 am",
+              textAlign: TextAlign.right,
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey,
               ),
-              Text(
-                "Somebody",
-                style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Text(
-                "Bot: This is a Message. It's just a dum...",
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16.0,
-                ),
-              ),
-            ],
-          ),
-          const Text(
-            "9:10 am",
-            textAlign: TextAlign.right,
+            ),
+          ],
+        ),
+        subtitle: Container(
+          padding: EdgeInsets.only(top: 5.0),
+          child: const Text(
+            "Bot: This is a Message. It's just a...",
             style: TextStyle(
-              fontSize: 12.0,
               color: Colors.grey,
+              fontSize: 16.0,
             ),
           ),
-        ],
+        ),
       ),
     );
 
